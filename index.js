e662375dceadb3f6c0d77216ca2fbe64f7b100eb
Nicko <nickojek2x@gmail.com>
2023-06-07 12:08:18 +0800

    resume and projects section added as well as carousel for projects images

--- index.js
+++ index.js
@@ -3,10 +3,57 @@ const showMenuRef = document.querySelector('#hame_menu');
 const hamItemsRef = document.querySelectorAll('.ham_item')
 const sectionsRef = document.querySelectorAll('section');
 const linksRef = document.querySelectorAll('.nav_link');
+const resumeLinks = document.querySelectorAll('.resume_link')
+const resumeRef = document.querySelectorAll('.res')
+const carouselRef = document.querySelectorAll('.carousel')
 
-console.log(sectionsRef)
-console.log(linksRef)
 
+function changeImageShow(images,current) {
+    images.forEach((image, index) => {
+        image.classList.remove('show-image')
+        image.classList.add('hide-image')
+
+        if(index === current){
+            image.classList.remove('hide-image')
+            image.classList.add('show-image')
+        }
+    })
+}
+
+carouselRef.forEach(carousel => {
+
+    const images = carousel.querySelectorAll('.project_image')
+    const prevBtn = carousel.querySelector('.prev')
+    const nextBtn = carousel.querySelector('.next')
+    
+
+    let current = 0;
+    const lastIndex = images.length - 1;
+
+    
+
+    prevBtn.addEventListener('click', function onPrevClick(){
+        console.log('prev')
+        if(current <= 0){
+            current = lastIndex;
+        }
+        else{
+            current--;
+        }
+        changeImageShow(images,current)
+    })
+
+    nextBtn.addEventListener('click', function onNextClick(){
+        console.log('next')
+        if(current >= lastIndex){
+            current = 0;
+        }
+        else{
+            current++;
+        }
+        changeImageShow(images,current)
+    })
+ })
 
 hamItemsRef.forEach(item => {
     item.addEventListener('click', function onHamItemClick() {
@@ -14,7 +61,6 @@ hamItemsRef.forEach(item => {
         showMenuRef.classList.add('close-menu');
     })
 })
-// console.log();
 menuRef.addEventListener('click', function onMenuClick() {
     if(showMenuRef.classList.contains('close-menu')){
         showMenuRef.classList.remove('close-menu');
@@ -24,7 +70,6 @@ menuRef.addEventListener('click', function onMenuClick() {
         showMenuRef.classList.remove('open-menu');
         showMenuRef.classList.add('close-menu');
     }
-
 });
 
 window.onresize = function onResize(){
@@ -37,6 +82,8 @@ window.onresize = function onResize(){
 
 window.onscroll = function onScroll(){
     let currentSection = "landing_page";
+    let currentRes = 'objective'
+
     sectionsRef.forEach(section => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
@@ -44,13 +91,22 @@ window.onscroll = function onScroll(){
             currentSection = section.getAttribute('id');
         }
     })
+    
+    resumeRef.forEach(res => {
+        const resTop = res.offsetTop;
+        const resHeight = res.clientHeight;
+        if(scrollY >= (resTop - resHeight / 3)){
+            currentRes =  res.getAttribute('id')
+        }
+    })
+
+
 
     linksRef.forEach(link => {
         link.classList.remove('activeNavLink');
         
         if(link.href.includes(currentSection)){
             link.classList.add('activeNavLink');
-            console.log(link.href)
         }
     })
 
@@ -59,7 +115,14 @@ window.onscroll = function onScroll(){
         
         if(item.href.includes(currentSection)){
             item.classList.add('activeHamItem');
-            console.log(item.href)
+        }
+    })
+
+    resumeLinks.forEach(link => {
+        link.classList.remove('activeResumeLink')
+        let linkRes = link.href.split('#')[1]
+        if(currentRes === linkRes){
+            link.classList.add('activeResumeLink')
         }
     })
 
